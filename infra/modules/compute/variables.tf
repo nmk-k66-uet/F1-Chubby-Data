903865f1210f74d11bfaa3a32e31d1a9980c32da
Duy Le <duy.le@optimizely.com>
2026-04-20 21:50:05 +0700

    change: infra setup for new streamlit model arch

--- infra/modules/compute/variables.tf
+++ infra/modules/compute/variables.tf
@@ -2,6 +2,10 @@ variable "project_id" {
   type = string
 }
 
+variable "region" {
+  type = string
+}
+
 variable "zone" {
   type = string
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,7 @@
 variable "project_id" {
   description = "GCP project ID"
   type        = string
-  default     = "gen-lang-client-0126315221"
+  default     = "gen-lang-client-0314607994"
 }
 
 variable "region" {
